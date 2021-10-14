028504591a562bf805b31d19cf6a65315c052370
mfacundoo <72402709+mfacundoo@users.noreply.github.com>
2021-10-14 13:55:14 -0300

    Add files via upload

---
+++ src/test/java/com/fernandofuentesfullstack/app/CursoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandofuentesfullstack.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
